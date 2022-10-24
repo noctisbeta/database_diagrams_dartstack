894fe4eeadd818b832838601796d4fcf3038b642
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-10-24 12:46:41 +0200

    added firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:database_diagrams/firebase_options.dart';
 import 'package:database_diagrams/main/main_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+// ...
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: MaterialApp(

cdcbe0b045aa8cb20647097dc3d59f51b79bb625
NoctisBeta <noctisbeta@gmail.com>
2025-05-05 17:01:43 +0200

    entity card ui update

--- client/lib/diagrams/components/entity_card.dart
+++ client/lib/diagrams/components/entity_card.dart
@@ -8,14 +8,15 @@ class EntityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => SizedBox(
-    width: 250, // Fixed width for the card
+    width: 250,
     child: Card(
       elevation: 4,
+      clipBehavior: Clip.antiAlias,
+      margin: EdgeInsets.zero,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: [
-          // Entity name section
           Container(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
             color: Theme.of(context).colorScheme.primaryContainer,
@@ -25,90 +26,90 @@ class EntityCard extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
-          // Attributes section
-          if (entity.attributes.isNotEmpty)
-            ColoredBox(
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  for (final attribute in entity.attributes)
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(color: Colors.grey.shade300),
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 6,
-                        horizontal: 16,
-                      ),
-                      child: Row(
-                        children: [
-                          // Name section with fixed width
-                          Expanded(flex: 2, child: Text(attribute.name)),
-                          // Flags section
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (attribute.isIdentity)
-                                const Padding(
-                                  padding: EdgeInsets.only(right: 4),
-                                  child: Icon(
-                                    Icons.format_list_numbered,
-                                    size: 16,
-                                    color: Colors.green,
-                                  ),
-                                ),
-                              if (attribute.isPrimaryKey)
-                                const Padding(
-                                  padding: EdgeInsets.only(right: 4),
-                                  child: Icon(
-                                    Icons.key,
-                                    size: 16,
-                                    color: Colors.amber,
-                                  ),
-                                ),
-                              if (attribute.isForeignKey)
-                                const Padding(
-                                  padding: EdgeInsets.only(right: 4),
-                                  child: Icon(
-                                    Icons.link,
-                                    size: 16,
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                            ],
-                          ),
-                          const SizedBox(width: 8),
-                          // Type section with fixed width
-                          Expanded(
-                            child: RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  color: Colors.grey.shade600,
-                                  fontStyle: FontStyle.italic,
-                                ),
-                                children: [
-                                  TextSpan(text: attribute.dataType),
-                                  if (attribute.isNullable)
-                                    const TextSpan(
-                                      text: '?',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.blue,
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+          ColoredBox(
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (final attribute in entity.attributes)
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(color: Colors.grey.shade300),
                       ),
                     ),
-                ],
-              ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 6,
+                      horizontal: 16,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(children: [Text(attribute.name)]),
+                        const SizedBox(height: 2),
+                        Row(
+                          children: [
+                            const Spacer(),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (attribute.isIdentity)
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 4),
+                                    child: Icon(
+                                      Icons.format_list_numbered,
+                                      size: 16,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                if (attribute.isPrimaryKey)
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 4),
+                                    child: Icon(
+                                      Icons.key,
+                                      size: 16,
+                                      color: Colors.amber,
+                                    ),
+                                  ),
+                                if (attribute.isForeignKey)
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 4),
+                                    child: Icon(
+                                      Icons.link,
+                                      size: 16,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            Text(
+                              attribute.dataType,
+                              style: TextStyle(
+                                color: Colors.grey.shade600,
+                                fontStyle: FontStyle.italic,
+                                fontSize: 12,
+                              ),
+                            ),
+                            if (attribute.isNullable)
+                              Padding(
+                                padding: const EdgeInsets.only(left: 4),
+                                child: Text(
+                                  '?',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue.shade700,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
             ),
+          ),
         ],
       ),
     ),
